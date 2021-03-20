82c96ef9a9d0fa3ac7ff2d365c3289c148d8b963
Arnaud Briard <arnaud.briard@edu.devinci.fr>
2021-03-20 15:48:26 +0100

    Book and Order with display

--- book.py
+++ book.py
@@ -17,40 +17,62 @@ class Book:
     def __init__(self,name,buy_orders,sell_orders):
         self.name=name
         self.buy_orders=buy_orders
-        self.sell_orders=sell_orders
-
-    def __str__(self, order_type):
+        self.sell_orders=sell_orders 
         
+    def __str__():
+        affichage="Book on %s" %(self.name)
+        for i in range(len(self.sell_orders)):
+            affichage+="\n\tSELL %s id=%d" %(Order(quantity,price),i)
+        for j in range (len(self.buy_orders)):
+            affichage+="\n\tBUY %s id=%d" %(Order(quantity,price),j)
+
     
     def insert_buy(self, quantity, price):
         insert = True
+        print ("Insert BUY %s id=%d on %s" %(Order(quantity,price),len(self.buy_orders)+1,self.name))
         for i in range(len(sell_orders)):
             if(sell_orders[i] < Order(quantity, price) or sell_orders[i].price == price):
                 if(sell_orders[i].quantity > quantity):
                     sell_orders[i].quantity -= quantity
                     insert = False
+                    print ("Execute %s on %s" %(Order(quantity,price),self.name))
                 elif(sell_orders[i].quantity == quantity):
                     del sell_orders[i]
                     insert = False
+                    print ("Execute %s on %s" %(Order(quantity,price),self.name))
                 else:
+                    executed_quantity=quantity
                     quantity -= sell_orders[i].quantity
                     del sell_orders[i]
+                    print ("Execute %s on %s" %(Order(executed_quantity,price),self.name))
         if(insert):
             self.buy_orders.append(Order(quantity, price))
+        print(str(Book(self.name,self.buy_orders,self.sell_orders)))
+
+
 
     def insert_sell(self, quantity, price):
         insert = True
+        print ("Insert SELL %s id=%d on %s" %(Order(quantity,price),len(self.sell_orders)+1,self.name))
         for i in range(len(buy_orders)):
             if(buy_orders[i] > Order(quantity, price) or buy_orders[i].price == price):
                 if(buy_orders[i].quantity > quantity):
                     buy_orders[i].quantity -= quantity
                     insert = False
+                    print ("Execute %s on %s" %(Order(quantity,price),self.name))
                 elif(buy_orders[i].quantity == quantity):
                     del buy_orders[i]
                     insert = False
+                    print ("Execute %s on %s" %(Order(quantity,price),self.name))
                 else:
+                    executed_quantity=quantity
                     quantity -= buy_orders[i].quantity
                     del buy_orders[i]
+                    print ("Execute %s on %s" %(Order(executed_quantity,price),self.name))
         if(insert):
             self.sell_orders.append(Order(quantity, price))
+        print(str(Book(self.name,self.buy_orders,self.sell_orders)))
+
+
+
 
